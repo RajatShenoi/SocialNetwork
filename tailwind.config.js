bfe415790f4a6a45cb76dc00cc20ec2c7d9c2296
Rajat Shenoi <rajatshenoi@outlook.com>
2023-10-02 14:08:49 +0530

    Installed TailwindCSS, DaisyUI and basic structure for templates

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html, js}",
+    "./accounts/templates/**/*.{html, js}",
+    "./static/**/*.{html, js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"),
+  ],
+}
+
