8a43ab57ff80a63715a717e00fff13dedcb6790d
praveen <cbsrirangapraveen.2004@gmail.com>
2023-10-11 19:17:36 +0530

    Re arranged the order and also removed the access to the admin to change the password

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-from accounts.models import User
+class CustomUserAdminForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(label=("Password"))
+    class Meta:
+        model = User
+        fields = '__all__'
 
-# Register your models here.
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserAdminForm
+    fieldsets = (
+        (None, {'fields': ('first_name', 'last_name', 'username', 'email', 'password')}),
+        ('Additional Info', {'fields': ('bio', 'avatar')}),
+        ('Permissions', {'fields': ('groups', 'user_permissions', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
