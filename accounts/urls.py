5e1dee80770ed7da8021985d03cd7ebc85ea080d
Rajat Shenoi <rajatshenoi@outlook.com>
2023-10-11 14:27:50 +0530

    Login functionality implemented

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='accounts-home'),
+    path('login/', views.loginUser, name='accounts-login'),
     path('register/', views.registerUser, name='accounts-register'),
     path('logout/', views.logoutUser, name='accounts-logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 from accounts.forms import MyUserCreationForm
+from accounts.models import User
 
 # Create your views here.
 def home(request):
@@ -24,6 +25,31 @@ def registerUser(request):
     return render(request, 'accounts/register.html', {
         'form': form
     })
+
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('accounts-home')
+    
+    if request.method == "POST":
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, 'User does not exist')
+            return render(request, 'accounts/login.html')
+        
+        user = authenticate(request, email=user.email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('accounts-home')
+        else:
+            messages.error(request, 'Email or password is incorrect')
+    
+    return render(request, 'accounts/login.html')
+        
 
 def logoutUser(request):
     logout(request)
